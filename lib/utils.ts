167d5cf34a44ab8ee4baddd370932381b9318bcf
DamaFace <marketingdamaface@gmail.com>
2025-10-29 11:18:50 -0300

    configs

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
@@ -8,3 +10,9 @@ export const newsletter = (email: string) => {
     
   return api.post("newsletter", { email }); 
 };
+
+
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
